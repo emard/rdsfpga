87f825717260f274a18136733827d48f4721c625
Emard <vordah@gmail.com>
2015-07-24 19:23:02 +0200

    adding Jacquet's stripped down rds message generator

---
+++ genmsg/rds.h
@@ -0,0 +1,45 @@
+/*
+    PiFmRds - FM/RDS transmitter for the Raspberry Pi
+    Copyright (C) 2014 Christophe Jacquet, F8FTK
+    
+    See https://github.com/ChristopheJacquet/PiFmRds
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RDS_H
+#define RDS_H
+
+
+#include <stdint.h>
+
+#define GROUP_LENGTH 4
+#define BITS_PER_GROUP (GROUP_LENGTH * (BLOCK_SIZE+POLY_DEG))
+
+/* The RDS error-detection code generator polynomial is
+   x^10 + x^8 + x^7 + x^5 + x^4 + x^3 + x^0
+*/
+#define POLY 0x1B9
+#define POLY_DEG 10
+#define MSB_BIT 0x8000
+#define BLOCK_SIZE 16
+
+extern void get_rds_samples(float *buffer, int count);
+extern void set_rds_pi(uint16_t pi_code);
+extern void set_rds_rt(char *rt);
+extern void set_rds_ps(char *ps);
+extern void set_rds_ta(int ta);
+extern void get_rds_group(int *buffer);
+
+#endif /* RDS_H */
---
+++ genmsg/rds_msg.c
@@ -0,0 +1,80 @@
+/*
+    PiFmRds - FM/RDS transmitter for the Raspberry Pi
+    Copyright (C) 2014 Christophe Jacquet, F8FTK
+    
+    See https://github.com/ChristopheJacquet/PiFmRds
+    
+    rds_wav.c is a test program that writes a RDS baseband signal to a WAV
+    file. It requires libsndfile.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sndfile.h>
+#include <string.h>
+
+#include "rds.h"
+
+#define NGROUPS 81
+
+/* Simple test program */
+int main(int argc, char **argv) {
+    static int bit_buffer[BITS_PER_GROUP];
+
+    if(argc < 4) {
+        fprintf(stderr, "Error: missing argument.\n");
+        fprintf(stderr, "Syntax: rds_wav <pid dec/hex> <short name> <longer message>\n");
+        return EXIT_FAILURE;
+    }
+    
+    set_rds_pi(strtoul(argv[1], NULL, 0));
+    set_rds_ps(argv[2]);
+    set_rds_rt(argv[3]);
+    printf("-- automatically generated with rds_msg\n");
+    printf("library ieee;\n");
+    printf("use ieee.std_logic_1164.all;\n");
+    printf("use ieee.std_logic_arith.all;\n");
+    printf("use ieee.std_logic_unsigned.all;\n");
+    printf("use ieee.numeric_std.all;\n");
+    printf("package message is\n");
+    printf("type rds_msg_type is array(0 to %d) of std_logic_vector(7 downto 0);\n", NGROUPS*13-1);
+    printf("constant rds_msg_map: rds_msg_type := (\n");
+    for(int i = 0; i < NGROUPS; i++)
+    {
+                get_rds_group(bit_buffer);
+                for(int j = 0; j < BITS_PER_GROUP; j += 8)
+                {
+                  uint8_t gbyte = 0;
+                  for(int k = 0; k < 8; k++)
+                  {
+                    gbyte <<= 1;
+                    if(bit_buffer[j+k])
+                      gbyte |= 1;
+                  }
+                  printf("x\"%02x\"", gbyte);
+                  // vhdl syntax: no last comma
+                  if(i != NGROUPS-1 || j != BITS_PER_GROUP-8)
+                    printf(",");
+                }
+                printf("\n");
+    }
+    printf(");\n");
+    printf("end message;\n");
+
+    return EXIT_SUCCESS;
+}
