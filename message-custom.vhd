7e611206e5e4fcf0e3a4bd78817c93bc18e03c00
Emard <vordah@gmail.com>
2015-07-25 14:15:28 +0200

    message size extended to 512 bytes to allocate more ram

--- message-custom.vhd
+++ message-custom.vhd
@@ -5,7 +5,7 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 package message is
-type rds_msg_type is array(0 to 259) of std_logic_vector(7 downto 0);
+type rds_msg_type is array(0 to 511) of std_logic_vector(7 downto 0);
 constant rds_msg_map: rds_msg_type := (
 x"ca",x"fe",x"a0",x"01",x"00",x"2e",x"8c",x"dc",x"da",x"cd",x"51",x"15",x"fb",
 x"ca",x"fe",x"a0",x"01",x"00",x"75",x"1c",x"dc",x"da",x"cd",x"4d",x"51",x"e9",
