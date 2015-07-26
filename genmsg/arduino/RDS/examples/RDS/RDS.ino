5a81fbfc02809b89440d63e9a2a45a5c251a9ab4
Emard <vordah@gmail.com>
2015-07-26 10:42:30 +0200

    updated example. Some bugs in library, strings incorrectly updated

--- genmsg/arduino/RDS/examples/RDS/RDS.ino
+++ genmsg/arduino/RDS/examples/RDS/RDS.ino
@@ -1,56 +1,48 @@
 /*
-    Copyright (C) Davor Jadrijevic
-    
-    See https://github.com/ChristopheJacquet/PiFmRds
-    
-    rds_wav.c is a test program that generates RDS
-    bit message in VHDL format.
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This is a test program that dynamically updates RDS message.
 */
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include <RDS.h>
 
 RDS rds = RDS();
 
 uint16_t pi = 0xCAFE;
-char ps[] = "TEST1234";
-char rt[] = "ZZZZZZZZZZZZZZZZZZZZZZZZ";
+char ps[9] = "TEST1234";
+char rt[65] = "ABCDEFGH";
 
-/* Simple test program */
 void setup() {
-
-    rds.set_rds_pi(pi);
-    rds.set_rds_ps(ps);
-    rds.set_rds_rt(rt);
-
-    Serial.begin(115200);
+  int i;
+  for(i = 0; i < sizeof(rt)-1; i++)
+    rt[i] = '@'+i; // ascii map
+  /* Setup initial RDS text */
+  rds.set_rds_pi(pi);
+  rds.set_rds_ps(ps);
+  rds.set_rds_rt(rt);
+  Serial.begin(115200);
 }
 
 void loop()
 {
-  static uint8_t i;
+  static uint8_t number;
 
-  i++;
-  rt[0] = 'A' + (i & 15);
-  rds.set_rds_rt(rt);  
-  rds.update();
-  Serial.println("RDS");
-  delay(5000);
+  snprintf(ps, sizeof(ps), "TEST%04d", number % 10000);
+  snprintf(rt, sizeof(rt), "%05d ZZZZZZZZZZZZZZZ", number % 100000);
+
+  // apply local strings to rds class
+  rds.set_rds_ps(ps);
+  rds.set_rds_ps(rt);
+
+  // strings changed but not yet transmitting
+  rds.update(); // update transmitter buffer
+
+  // print actual status on serial
+  Serial.print("0x");
+  Serial.print(pi, HEX);
+  Serial.print(" ");
+  Serial.print(ps);
+  Serial.print(" ");
+  Serial.println(rt);
+
+  delay(1000); // wait 1 second
+  number++; // increment number
 }
